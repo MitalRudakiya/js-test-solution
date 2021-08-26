7452db6a626dc4c0d3580bc0fb03da66ee850593
MitalRudakiya <89589851+MitalRudakiya@users.noreply.github.com>
2021-08-27 02:49:03 +0530

    Adding solution for task 5

--- js-test-master/tasks/5. Caesar cipher/caesar.js
+++ js-test-master/tasks/5. Caesar cipher/caesar.js
@@ -1,3 +1,14 @@
 export function encryptCaesar(inputString, key) {
-  // TODO: write your code here
+  inputString = inputString.toUpperCase();
+  var firstCode = 65, lastCode = 90;
+  var outputStr = "";
+  for(let i = 0; i < inputString.length; i++){
+    var code = inputString.charCodeAt(i);
+    var nextCode = code + key;
+    if(nextCode > lastCode){
+      nextCode = firstCode + (nextCode - lastCode - 1);
+    }
+    outputStr += String.fromCharCode(nextCode);
+  }
+  return outputStr;
 }
