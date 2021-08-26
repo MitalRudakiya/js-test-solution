1f257cb30fbb44fa417406391ade0e055db80ba6
MitalRudakiya <89589851+MitalRudakiya@users.noreply.github.com>
2021-08-27 02:22:56 +0530

    Adding solution for Task 4

--- js-test-master/tasks/4. FizzBuzz/fizzbuzz.js
+++ js-test-master/tasks/4. FizzBuzz/fizzbuzz.js
@@ -1,3 +1,17 @@
 export function getFizzBuzzUntil(n) {
-  // TODO: write your code here
+  if(n==1){
+    return [1];
+  } else {
+    var result = getFizzBuzzUntil(n-1);
+    var nextmember = n;
+    if(n%3 == 0 && n%5 == 0){
+      nextmember = "FizzBuzz";
+    } else if(n%5 == 0){
+      nextmember = "Buzz";
+    } else if(n%3 == 0){
+      nextmember = "Fizz";
+    }
+    result.push(nextmember);
+    return result;
+  }
 }
