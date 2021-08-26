0f16c52d0bef266782504f97477f756e37a25e7a
MitalRudakiya <89589851+MitalRudakiya@users.noreply.github.com>
2021-08-27 03:10:27 +0530

    Adding solution for task 7

--- js-test-master/tasks/7. Is Point inside Circle/Circle.js
+++ js-test-master/tasks/7. Is Point inside Circle/Circle.js
@@ -11,7 +11,13 @@ export default function(center, radius) {
       return radius ** 2 * Math.PI;
     },
     includes(Point) {
-      // TODO: write your code here
+      if ((Point.x - center.x) * (Point.x - center.x) +
+        (Point.y - center.y) * (Point.y - center.y) <= radius * radius){
+        return true;
+      }
+      else {
+          return false;
+      }
     }
   }
 }
