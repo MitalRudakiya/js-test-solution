519e386a0c0de1d3d2c0f7da2e45935a0bf9c3c8
MitalRudakiya <89589851+MitalRudakiya@users.noreply.github.com>
2021-08-27 01:52:05 +0530

    Add solution for task 2

--- js-test-master/tasks/2. Fibonacci - recursive/fibonacci.js
+++ js-test-master/tasks/2. Fibonacci - recursive/fibonacci.js
@@ -1,3 +1,14 @@
 export function getFibonacciUntil(n) {
-  //TODO write your code here, remember it should be recursive
+  if(n == 0){
+    return [];
+  }else if(n == 1){
+    return [0];
+  } else if(n == 2){
+    return [0 , 1];
+  } else {
+    var result = getFibonacciUntil(n-1);
+    var nextmember = result[n-2] + result[n-3];
+    result.push(nextmember)
+    return result;
+  }
 }
