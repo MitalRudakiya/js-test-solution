f949f4b8b0484c114875b89b6c458efde9241486
MitalRudakiya <89589851+MitalRudakiya@users.noreply.github.com>
2021-08-27 02:07:13 +0530

    Adding solution for task 3

--- js-test-master/tasks/3. Balanced parentheses/balancedParentheses.js
+++ js-test-master/tasks/3. Balanced parentheses/balancedParentheses.js
@@ -1,3 +1,17 @@
 export function areParenthesesBalanced(inputString) {
-  // TODO: write your code here
+  var countopen = 0, countclose = 0;
+  for(let i =0; i< inputString.length; i++){
+    if(inputString[i] == '('){
+      countopen++;
+    } else if(inputString[i] == ')'){
+      countclose++;
+    }
+    if(countclose > countopen){
+      return false;
+    }
+  }
+  if(countclose != countopen){
+    return false;
+  }
+  return true;
 }
